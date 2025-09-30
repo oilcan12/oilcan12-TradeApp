c7a72aa80aacda537b21d77e75585cbd86529579
oilcan12 <98504791+oilcan12@users.noreply.github.com>
2025-09-30 16:24:32 -0400

    Delete src directory

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import EquipmentValuationApp from "./EquipmentValuationApp";
-ReactDOM.render(<EquipmentValuationApp />, document.getElementById("root"));
